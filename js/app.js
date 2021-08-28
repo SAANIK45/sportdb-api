2f541141effde815b9f03e95d10f8354d178761c
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-08-29 01:59:41 +0600

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 const loadData = () =>{
-    
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = ' ';
+    const url = `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${searchText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => getData(data.teams[0]))
+}
+
+const getData = (data) =>{
+    console.log(data)
+    const sportContainer = document.getElementById('sport-container');
+    sportContainer.textContent = ' ';
+    const div = document.createElement('div');
+    div.classList = 'sportTeam';
+    div.innerHTML = `
+        <img src="${data.strTeamBadge}" class="card-img" alt="...">
+        <div class="card">
+            <h5 class="card-title">${data.strCountry}</h5>
+            <p class="card-text">${data.strDescriptionEN}</p>
+            <p class="card-text">${data.strWebsite}</p>
+        </div>
+    `
+    sportContainer.appendChild(div);
 }
