ff38bed83ced6008be3b33d145daa439b34eb5fb
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-08-29 13:52:59 +0600

    update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 const loadData = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    searchField.value = ' ';
-    const url = `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${searchText}`;
+    console.log(searchText);
+    const url=`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${searchText}`;
     fetch(url)
     .then(res => res.json())
     .then(data => getData(data.teams[0]))
+    searchField.value = ' ';
+
 }
 
 const getData = (data) =>{
@@ -15,11 +17,14 @@ const getData = (data) =>{
     const div = document.createElement('div');
     div.classList = 'sportTeam';
     div.innerHTML = `
-        <img src="${data.strTeamBadge}" class="card-img" alt="...">
-        <div class="card">
-            <h5 class="card-title">${data.strCountry}</h5>
-            <p class="card-text">${data.strDescriptionEN}</p>
-            <p class="card-text">${data.strWebsite}</p>
+        <div class="sport-images">
+            <img src="${data.strTeamBadge}" alt="...">
+            <img src="${data.strTeamJersey}" alt="...">
+        </div> 
+        <div class="sport-details">
+            <h5>${data.strCountry}</h5>
+            <p>${data.strDescriptionEN}</p>
+            <p>${data.strWebsite}</p>
         </div>
     `
     sportContainer.appendChild(div);
